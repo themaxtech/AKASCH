5521123ffd2c35549ac54ca76668c9949cec9a99
themaxtech <maxtechapps@gmail.com>
2016-06-11 16:18:08 +0530

    fe
    wsdasd

--- www/js/index.js
+++ www/js/index.js
@@ -217,12 +217,12 @@ $(document).on('pagecontainershow', function (e, ui) {
                 $.jStorage.set("username", userHandler.username);
 
                 arung7   = $.jStorage.get("appid");
-                //arung111 = $.jStorage.get("gcmid");
+                arung111 = $.jStorage.get("gcmid");
                 arung222 = $.jStorage.get("wpnid");
                 newarun =  encodeURIComponent($.jStorage.get("wpnid"));
                 userHandler.appid = $.jStorage.get("appid"); 
-                //userHandler.gcmid = $.jStorage.get("gcmid"); 
-                //userHandler.wpnid = $.jStorage.get("wpnid"); 
+                userHandler.gcmid = $.jStorage.get("gcmid"); 
+                userHandler.wpnid = $.jStorage.get("wpnid"); 
                 //Send data to server through the Ajax call
                 //action is functionality we want to call and outputJSON is our data
                     //$.ajax({url: 'http://akaraeducation.in/ios/auth.php',
