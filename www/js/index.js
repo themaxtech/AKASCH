6c7b7c14b18c0c8a2eddbf71d8f3237408bb84bb
themaxtech <maxtechapps@gmail.com>
2016-06-13 13:19:45 +0530

    sadw2q
    3423

--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-/*
-$.jStorage.set("username", '');
-$.jStorage.set("status", '');
-$.jStorage.set("userpass", '');
-$.jStorage.set("userclass", '');
-$.jStorage.set("usersec", '');
-$.jStorage.set("useroriname", '');
-*/
-
-
 var arung1 = '';
 var arung2 = '';
 var arung3 = '';
@@ -2112,8 +2083,7 @@ $(document).on('pageshow', '#homepage', function(){
         arung6 = $.jStorage.get("useroriname"); 
         arung7 = $.jStorage.get("appid");  
         arung111 = $.jStorage.get("gcmid");
-        arung222 = $.jStorage.get("wpnid");  
-        
+        arung222 = $.jStorage.get("wpnid");   
 
         userHandler.username    = arung1;
         userHandler.userpass    = arung3;
@@ -2372,5 +2342,3 @@ $(document).off('click', '#leavelistsubmit').on('click', '#leavelistsubmit',func
     
 
 });
- 
- 
